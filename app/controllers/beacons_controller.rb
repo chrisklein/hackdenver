30c8c296816c2e5f0ed72fe560c36b03305e9fdc
Dan Logan <dlogan21@gmail.com>
2012-08-18 10:29:14 -0600

    renders in beacons

--- app/controllers/beacons_controller.rb
+++ app/controllers/beacons_controller.rb
@@ -9,15 +9,15 @@ class BeaconsController < ApplicationController
 
   def create
     if not params[:lat].blank? and not params[:long].blank? and not params[:description].blank? and not params[:duration].blank?
-      Beacon.create(
+      beacon = Beacon.create(
           :lat => params[:lat],
           :long => params[:long],
           :description => params[:description],
           :duration => params[:duration]
       )
-      render :text =>  {:status => 'success'}.to_json, :content_type => 'application/json'
+      render :text => {:status => 'success', :beacon_id => beacon.id.to_s}.to_json, :content_type => 'application/json'
     else
-      render :text =>  {:status => 'failure'}.to_json, :content_type => 'application/json'
+      render :text => {:status => 'failure'}.to_json, :content_type => 'application/json'
     end
   end
 
@@ -26,10 +26,10 @@ class BeaconsController < ApplicationController
       if beacon = Beacon.first(:conditions => ['id = ?', params[:id]])
         render :text => {:status => 'success', :description => beacon.description, :duration => beacon.duration, :created_at => beacon.created_at}.to_json, :content_type => 'application/json'
       else
-        render :text =>  {:status => 'failure'}.to_json, :content_type => 'application/json'
+        render :text => {:status => 'failure'}.to_json, :content_type => 'application/json'
       end
     else
-      render :text =>  {:status => 'failure'}.to_json, :content_type => 'application/json'
+      render :text => {:status => 'failure'}.to_json, :content_type => 'application/json'
     end
   end
 
@@ -37,8 +37,12 @@ class BeaconsController < ApplicationController
     if not params[:id].blank?
       if beacon = Beacon.first(['id = ?', params[:id]])
         beacon.destroy
-        render :text =>  {:status => 'success'}.to_json, :content_type => 'application/json'
+        render :text => {:status => 'success'}.to_json, :content_type => 'application/json'
+      else
+        render :text => {:status => 'failure'}.to_json, :content_type => 'application/json'
       end
+    else
+      render :text => {:status => 'failure'}.to_json, :content_type => 'application/json'
     end
   end
 

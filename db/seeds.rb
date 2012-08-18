1f4ce68fbcd6f064988ad4a6f2e12f89941d782d
Cory LaNou <cory@joinlocal.com>
2012-08-17 22:25:24 -0600

    potential location seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+#
+#
+
+#  Novo Coffee
+#  3008 Larimer St 
+#  Denver CO 80205
+#  Latitude: 39.762459
+#  Longitude: 104.980738
+#  
+#  Crema Coffee House
+#  2862 Larimer St 
+#  Denver CO 80205
+#  Latitude: 39.761055
+#  Longitude: 104.982477
+#  
+#  Mercury Cafe
+#  2199 California St 
+#  Denver CO 80205
+#  Latitude: 39.750686
+#  Longitude: 104.985136
+#  
+#  Meadowlark Bar
+#  2701 Larimer St 
+#  Denver CO 80205
+#  Latitude: 39.759586 
+#  Longitude: 104.984436
+#
+#  Flying Dog Brewery
+#  2330 Broadway 
+#  Denver CO 80205
+#  Latitude: 39.750972
+#  Longitude: 104.987671 

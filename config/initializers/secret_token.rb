be12c1856ed6a2781aedaff50aa494e20cb39185
Levi Cook <levicook@gmail.com>
2012-08-17 21:20:46 -0600

    Basic rails app, talking with MySQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackdenver::Application.config.secret_token = 'feb791ab6ce6a02a6b58585ffd7061f2fb3585eec3a8ce4a390de453babe549b5e656a680b369280d2ccb921625c458f246af68398ec152ec992a553ad1ce7ac'
